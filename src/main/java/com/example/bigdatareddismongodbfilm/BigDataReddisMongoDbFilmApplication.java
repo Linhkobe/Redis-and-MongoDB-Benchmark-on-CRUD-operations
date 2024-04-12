d7417a2fb84cb1eda38b10155538da37e791b5c4
jeremie-brezzo <jeremiebrezzo@gmail.com>
2024-04-12 14:48:57 +0200

    project setup

---
+++ src/main/java/com/example/bigdatareddismongodbfilm/BigDataReddisMongoDbFilmApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigdatareddismongodbfilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDataReddisMongoDbFilmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigDataReddisMongoDbFilmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bigdatareddismongodbfilm/BigDataReddisMongoDbFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bigdatareddismongodbfilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigDataReddisMongoDbFilmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
