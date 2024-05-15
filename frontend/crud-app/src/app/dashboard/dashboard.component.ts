53a54f8af0eb282ae56967b07ea87fca4b660a01
HASSADZAKARIA <hassad.zakaria04@gmail.com>
2024-05-15 13:52:37 +0200

    relier les points

--- frontend/crud-app/src/app/dashboard/dashboard.component.ts
+++ frontend/crud-app/src/app/dashboard/dashboard.component.ts
@@ -9,7 +9,8 @@ import { SharedService } from '../shared.service';
 })
 export class DashboardComponent implements OnInit {
   chart: any;
-  benchmarkCount: number = 0;
+  prevMongoValue: number = 0;
+  prevRedisValue: number = 0;
 
   constructor(private sharedService: SharedService) {
     Chart.register(...registerables); // Assurez-vous d'enregistrer les composants Chart.js requis
@@ -19,73 +20,63 @@ export class DashboardComponent implements OnInit {
     this.initChart();
     this.sharedService.benchmarkData$.subscribe(data => {
       if (data) {
-        this.benchmarkCount++;
-        this.addChartData(data, this.benchmarkCount);
+        this.addChartData(data);
       }
     });
   }
 
-  addChartData(data: any, benchmarkCount: number) {
-    // Ajoutez la valeur Mongo comme point bleu
+  addChartData(data: any) {
+    const benchmarkCount = this.chart.data.labels.length + 1;
+
+    // Ajouter les valeurs MongoDB et Redis avec une ligne reliant les points
+    this.chart.data.labels.push(`Benchmark ${benchmarkCount}`);
     this.chart.data.datasets[0].data.push({
       x: benchmarkCount,
-      y: data.averageTimeElapsedInMillisecondsMongo,
-      backgroundColor: 'blue'
+      y: data.averageTimeElapsedInMillisecondsMongo
     });
-
-    // Ajoutez la valeur Redis comme point rouge
     this.chart.data.datasets[1].data.push({
       x: benchmarkCount,
-      y: data.averageTimeElapsedInMillisecondsRedis,
-      backgroundColor: 'red'
+      y: data.averageTimeElapsedInMillisecondsRedis
     });
 
-    // Ajouter une ligne reliant les points
     if (benchmarkCount > 1) {
-      const prevBenchmarkCount = benchmarkCount - 1;
+      // Relier la valeur actuelle avec la valeur précédente pour MongoDB
       this.chart.data.datasets[0].data.push({
-        x: prevBenchmarkCount,
-        y: data.averageTimeElapsedInMillisecondsMongo,
-        backgroundColor: 'transparent'
+        x: benchmarkCount,
+        y: this.prevMongoValue
       });
+      // Relier la valeur actuelle avec la valeur précédente pour Redis
       this.chart.data.datasets[1].data.push({
-        x: prevBenchmarkCount,
-        y: data.averageTimeElapsedInMillisecondsRedis,
-        backgroundColor: 'transparent'
+        x: benchmarkCount,
+        y: this.prevRedisValue
       });
     }
+
+    this.prevMongoValue = data.averageTimeElapsedInMillisecondsMongo;
+    this.prevRedisValue = data.averageTimeElapsedInMillisecondsRedis;
 
     this.chart.update();
   }
 
   initChart(): void {
     this.chart = new Chart('canvas', { // Assurez-vous que l'ID 'canvas' existe dans votre HTML
-      type: 'scatter',
+      type: 'line',
       data: {
         datasets: [
           {
             label: 'MongoDB',
             data: [],
-            pointRadius: 5,
-            pointHoverRadius: 8,
-            pointStyle: 'circle',
             borderColor: 'blue',
-            backgroundColor: 'transparent',
-            showLine: true,
-            fill: false,
+            fill: false
           },
           {
             label: 'Redis',
             data: [],
-            pointRadius: 5,
-            pointHoverRadius: 8,
-            pointStyle: 'rectRot',
             borderColor: 'red',
-            backgroundColor: 'transparent',
-            showLine: true,
-            fill: false,
+            fill: false
           }
-        ]
+        ],
+        labels: [] // Les labels seront ajoutés dynamiquement à chaque nouveau benchmark
       },
       options: {
         responsive: true,
