ef423f5ea1a8eee2fa4b2f65d99bbb33cee10377
ZaherHicham <hicham.zaher@etu.unice.fr>
2024-05-15 12:57:07 +0200

    affichage du dashboard

--- frontend/crud-app/src/app/dashboard/dashboard.component.ts
+++ frontend/crud-app/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
-// dashboard.component.ts
 import { Component, OnInit } from '@angular/core';
-import { Chart } from 'chart.js';
+import { Chart, registerables } from 'chart.js';
 import { SharedService } from '../shared.service';
 
 @Component({
@@ -11,18 +10,47 @@ import { SharedService } from '../shared.service';
 export class DashboardComponent implements OnInit {
   chart: any;
 
-  constructor(private sharedService: SharedService) { }
+  constructor(private sharedService: SharedService) {
+    Chart.register(...registerables); // Make sure to register the required Chart.js components
+  }
 
   ngOnInit(): void {
+    this.initChart();
     this.sharedService.benchmarkData$.subscribe(data => {
       if (data) {
-        this.chart.data.datasets[0].data.push(data.averageTimeElapsedInMillisecondsRedis);
-        this.chart.data.datasets[1].data.push(data.averageTimeElapsedInMillisecondsMongo);
+        this.addChartData(data);
+      }
+    });
+    this.sharedService.benchmarkData$.subscribe(data => {
+      if (data) {
+        const runIndex = this.chart.data.labels.length + 1;
+        this.chart.data.labels.push(runIndex); // Ajoutez l'index comme label
+        this.chart.data.datasets[0].data.push({
+          x: runIndex,
+          y: data.averageTimeElapsedInMillisecondsRedis
+        });
+        this.chart.data.datasets[1].data.push({
+          x: runIndex,
+          y: data.averageTimeElapsedInMillisecondsMongo
+        });
         this.chart.update();
       }
     });
+  }
 
-    this.chart = new Chart('canvas', {
+  addChartData(data: any) {
+    const newLabelIndex = this.chart.data.labels.length + 1;
+    this.chart.data.labels.push(`Run ${newLabelIndex}`);
+    this.chart.data.datasets[0].data.push(data.mongoTime);
+    this.chart.data.datasets[1].data.push(data.redisTime);
+    this.chart.update();
+  }
+
+  initChart(): void {
+    if (this.chart) {
+      this.chart.destroy();
+    }
+    this.chart = new Chart('canvas', { // Ensure the ID 'canvas' exists in your HTML
       type: 'line',
       data: {
         datasets: [
@@ -38,17 +66,22 @@ export class DashboardComponent implements OnInit {
             borderColor: 'blue',
             fill: false
           }
-        ]
+        ],
+        labels: [] // Consider adding labels if needed
       },
       options: {
+        responsive: true,
+        maintainAspectRatio: false,
         scales: {
-          x: { // Use 'x' instead of 'xAxes'
+          x: {
             type: 'linear',
             position: 'bottom',
+          },
+          y: { // Ensure there is a 'y' scale configuration if needed
             beginAtZero: true
           }
         }
-      }
+      },
     });
   }
 }
